9c99586547ecf9da646c78db158a6d94cb9ac8f3
aydin827 <13597@holbertonstudents.com>
2026-04-09 19:53:06 +0000

    TASK2:f

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    first = sentence[0] if length > 0 else None
-    return (length, first) 
+    if length == 0:
+        first_char = None
+    else:
+        first_char = sentence[0]
+    
+    return (length, first_char)
