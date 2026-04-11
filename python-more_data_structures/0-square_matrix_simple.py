56662e5eddff6b18987ff59a0cd929aca0db5b90
aydin827 <13597@holbertonstudents.com>
2026-04-11 08:19:20 +0000

    TASK0:g

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    for row in matrix:
+        new_matrix[]
+        for num in row:
+            new_row.append(num * num)
+        new_matirx.append(new_row)
+    return new_matrix
