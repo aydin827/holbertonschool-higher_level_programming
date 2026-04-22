f1538e375af466549340e0467fea9fccc9deb7da
aydin827 <13597@holberhonstudents.com>
2026-04-22 18:42:49 +0000

    TASK1:f

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -2,5 +2,5 @@
 import sys
 import urllib.request
 url = sys.argv[1]
-with urllib.request.urlopen(url) as response
-print(response.headers.get("X-Request-Id"))
+with urllib.request.urlopen(url) as response:
+    print(response.headers.get("X-Request-Id"))
