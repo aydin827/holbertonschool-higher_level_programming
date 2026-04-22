ad02bc951593f07698a29d1c1b06d921518e43ac
aydin827 <13597@holberhonstudents.com>
2026-04-22 18:26:21 +0000

    TASK0:f

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,7 +2,7 @@
 import urllib.request
 url = "https://intranet.hbtn.io/status"
 with urllib.request.urlopen(url) as response:
-    body = response.body()
+    body = response.read()
     print("Body response:")
     print("\t- type:", type(body))
     print("\t- content:", body)
