4ce6713c0e10eeea5ea8093b4e44750826d6cc3f
aydin827 <13597@holbertonstudents.com>
2026-04-23 20:11:04 +0000

    TASK7:f

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -5,12 +5,9 @@ If the HTTP status code is >= 400, prints the error code.
 """
 import requests
 import sys
-
-
 if __name__ == "__main__":
     url = sys.argv[1]
-    response = requests.get(url)
-    
+    response = requests.get(url) 
     if response.status_code >= 400:
         print("Error code: {}".format(response.status_code))
     else:
