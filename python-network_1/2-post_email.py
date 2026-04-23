1f277fbc251bf3c39a2730e07ec2911a6cb5fe82
aydin827 <13597@holbertonstudents.com>
2026-04-23 18:52:01 +0000

    TASK2:f

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import urllib.request
+import sys
+import urllib.parse
+url = sys.argv[1]
+email = sys.argv[2]
+data = urllib.parse.urlencode({'email' : email})
+data = data.encode('ascii')
+with urllib.request.urlopen(url, data) as response:
+    body = response.read()
+print(body.decode("utf-8"))
