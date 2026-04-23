8d01b5c8b8a4d6e6258a259b1e9514d30db350e9
aydin827 <13597@holbertonstudents.com>
2026-04-23 19:30:18 +0000

    TASK3:f

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -4,10 +4,12 @@ f
 """
 import urllib.request
 import sys
-url = sys.argv[1]
-try:
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
-    print(body.decode("utf-8"))
-except urllib.error.HTTPError as e:
-    print("Error code:", e.code)
+import urllib.error
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read()
+            print(body.encode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)
