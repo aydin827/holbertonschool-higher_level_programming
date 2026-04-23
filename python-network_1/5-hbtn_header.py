6d91d6360c67799180ffbda0cd51f780e353683b
aydin827 <13597@holbertonstudents.com>
2026-04-23 19:55:11 +0000

    TASK5:f

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""
+f
+"""
 import requests
 import sys
-url = sys.argv[1]
-response = requests.get(url)
-print(content.headers.get("X-Request-Id"))
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    print(content.headers.get("X-Request-Id"))
