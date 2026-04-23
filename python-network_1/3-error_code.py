04ce8c4989f614897cb65244ed2396ff59962c4a
aydin827 <13597@holbertonstudents.com>
2026-04-23 19:33:34 +0000

    TASK3:f

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     try:
         with urllib.request.urlopen(url) as response:
             body = response.read()
-            print(body.encode("utf-8"))
+            print(body.decode("utf-8"))
     except urllib.error.HTTPError as e:
         print("Error code:", e.code)
