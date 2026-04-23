89b00ae2b69ef671b32c54b6d1d422d770a1f44f
aydin827 <13597@holbertonstudents.com>
2026-04-23 20:05:49 +0000

    TASK6:f

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -7,6 +7,6 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    response = requests.post(url, data={'email'= email})
+    response = requests.post(url, data={'email': email})
     print(response.text)
 
