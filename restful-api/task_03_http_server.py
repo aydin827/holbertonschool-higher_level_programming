37684c149f03eaacaf242ae04f5e9873ae82c6cb
aydin827 <13597@holbertonstudents.com>
2026-04-25 14:10:40 +0000

    TASK3:f

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,24 +1,31 @@
 #!/usr/bin/python3
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
+
+
 class MyHandler(BaseHTTPRequestHandler):
-    def do_get(self):
+
+    def do_GET(self):
+
         if self.path == "/":
             self.send_response(200)
-            self.send_header("Content-type", "test/plain")
+            self.send_header("Content-type", "text/plain")
             self.end_headers()
-        self.wfile.write(b"Hello, this is a simple API!")
-     elif self.path == "/data":
+
+            self.wfile.write(b"Hello, this is a simple API!")
+        elif self.path == "/data":
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
-              data = {
+
+            data = {
                 "name": "John",
                 "age": 30,
                 "city": "New York"
             }
-             self.wfile.write(json.dumps(data).encode())
-     elif self.path == "/status":
+
+            self.wfile.write(json.dumps(data).encode())
+        elif self.path == "/status":
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
@@ -26,8 +33,6 @@ class MyHandler(BaseHTTPRequestHandler):
             data = {"status": "OK"}
 
             self.wfile.write(json.dumps(data).encode())
-
-        # 4. NOT FOUND (error handling)
         else:
             self.send_response(404)
             self.send_header("Content-type", "application/json")
