851b1ce58fbd43122424881ec93dea333bf9faca
aydin827 <13597@holbertonstudents.com>
2026-04-25 10:32:07 +0000

    TASK10:f

---
+++ python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+d
+"""
+import requests
+import sys
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    url = "https://api.github.com/user"
+    auth = (username, password)
+    response = requests.get(url, auth=(username, password))
+    data = response.json()
+    print(data.get("id"))
